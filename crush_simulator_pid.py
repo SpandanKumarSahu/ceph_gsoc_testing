f82d3b4ccc0c6ba700364218d426246b4f9d40d8
Spandan Kumar Sahu <spandankumarsahu@gmail.com>
2017-05-18 19:04:49 +0530

    add pid implementation

--- crush_simulator_pid.py
+++ crush_simulator_pid.py
@@ -20,11 +20,25 @@ def reweight_after_replica_write():
 	return	
 
 def reweight_after_object_write():
-	current_load_difference = [x - y for x, y in zip(normalise(current_load_distribution), norm_weights)]
-	max_value = max(current_load_difference)
-	min_value = min(current_load_difference)
-	current_weights[current_load_difference.index(max_value)] *= math.pow(1-replica_count*0.01, replica_count)
-	current_weights[current_load_difference.index(min_value)] *= math.pow(1+replica_count*0.01, replica_count)
+	global current_load_distribution
+	global norm_weights
+	norm_current_load_distribution = normalise(current_load_distribution)
+	global current_load_difference
+	current_load_difference = [y - x for x, y in zip(norm_current_load_distribution, norm_weights)]
+	global sum_load_difference
+	sum_load_difference = [x + y for x, y in zip(current_load_difference, sum_load_difference)]
+	global diff_load_difference
+	diff_load_difference = [y - x for x, y in zip(norm_current_load_distribution, past_load_difference)]
+	global past_load_difference
+	past_load_difference = current_load_difference[:]	
+	
+	total_error = [ kp*x+ki*y+kd*z for x, y, z in zip(current_load_difference, sum_load_difference, diff_load_difference)]	
+	total_error = [math.fabs(i) for i in total_error]
+	norm_total_error = normalise(total_error)	
+
+	global current_weights
+	for i in range(num_OSD):
+		current_weights[i] *= (1 + total_error[i] * norm_total_error[i])  
 
 def getWeights( checklist ):
 	weight_list = [current_weights[i] for i in checklist]
@@ -56,6 +70,13 @@ input_weights()
 count_objects = 0
 
 current_load_distribution = [0]*num_OSD
+sum_load_difference = [0]*num_OSD
+diff_load_difference = normalise(weights)
+past_load_difference = [0]*num_OSD
+kp = 0.8
+ki = 0.1
+kd = 0.00
+
 current_weights = weights[:]
 norm_weights = normalise(weights)
 
@@ -67,7 +88,6 @@ while (count_objects < 10000):
 		reweight_after_replica_write()
 		picks += 1
 	reweight_after_object_write()
-	count_objects += 1
-	if count_objects > 9000:	
-		print normalise(current_load_distribution)
+	count_objects += 1	
+	print normalise(current_load_distribution)
 
